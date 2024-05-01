e0afebf0e40efc549b137b2ea9b9e39f755893a7
sdfowers <sdfowers@gmail.com>
2024-05-01 21:28:45 +1000

    finishing the navbar

--- src/Components/FoodDelivery/NavBar/Navbar.jsx
+++ src/Components/FoodDelivery/NavBar/Navbar.jsx
@@ -9,10 +9,10 @@ export const Navbar = () => {
     <div className='navbar'>
         <img src={assets.logo} alt='' className='logo'/>
         <ul className="navbar-menu">
-            <li className={menu==="home"?"active":""}>Home</li>
-            <li className={menu==="menu"?"active":""}>Menu</li>
-            <li className={menu==="mobile-app"?"active":""}>Mobile-app</li>
-            <li className={menu==="contact-us"?"active":""}>Contact us</li>
+            <li onClick={() => setMenu("home")} className={menu==="home"?"active":""}>Home</li>
+            <li onClick={() => setMenu("menu")} className={menu==="menu"?"active":""}>Menu</li>
+            <li onClick={() => setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>Mobile-app</li>
+            <li onClick={() => setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contact us</li>
         </ul>
         <div className="navbar-right">
             <img src={assets.search_icon} alt="" />
