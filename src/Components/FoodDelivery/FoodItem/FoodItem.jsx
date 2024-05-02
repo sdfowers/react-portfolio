1bb343a2d2e5dd133e83757e140a75d28aa0c889
sdfowers <sdfowers@gmail.com>
2024-05-02 17:10:35 +1000

    fooditem styling

--- src/Components/FoodDelivery/FoodItem/FoodItem.jsx
+++ src/Components/FoodDelivery/FoodItem/FoodItem.jsx
@@ -3,6 +3,8 @@ import './FoodItem.css'
 import { assets } from '../assets/assets'
 
 const FoodItem = ({id, name, price, description, image}) => {
+    
+
   return (
     <div className='food-item'>
         <div className="food-item-image-container">
