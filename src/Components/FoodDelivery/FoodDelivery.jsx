2bd28ee338afec274c887204336bf7ef0ae268f1
sdfowers <sdfowers@gmail.com>
2024-05-05 15:12:15 +1000

    Added footer component

--- src/Components/FoodDelivery/FoodDelivery.jsx
+++ src/Components/FoodDelivery/FoodDelivery.jsx
@@ -3,6 +3,7 @@ import { Navbar } from './NavBar/Navbar'
 import { Home } from './Pages/Home/Home'
 import './FoodDelivery.css'
 import StoreContextProvider from './Context/StoreContext'
+import Footer from './Footer/Footer'
 
 export const FoodDelivery = () => {
   return (
@@ -11,6 +12,7 @@ export const FoodDelivery = () => {
         <StoreContextProvider>
           	<Home></Home>
         </StoreContextProvider>
+        <Footer></Footer>
     </div>
   )
 }
